df1476a81ac5e7f4075caa2a58e983fe1b47316e
Amod <amod.chaubey@acc.ltd>
2023-03-28 17:25:09 +0530

    exceptionHandling

--- example/test.js
+++ example/test.js
@@ -1,130 +1,133 @@
-let { SmartAPI, WebSocketClient } = require("smartapi-javascript");
+let { SmartAPI, WebSocketClient, WebSocketV2 } = require('../lib');
 
 let smart_api = new SmartAPI({
-    api_key: "smartapi_key",    // PROVIDE YOUR API KEY HERE
-    // OPTIONAL : If user has valid access token and refresh token then it can be directly passed to the constructor 
-    // access_token: "YOUR_ACCESS_TOKEN",
-    // refresh_token: "YOUR_REFRESH_TOKEN"
+	api_key: 'm7TeW9kY',
+	// OPTIONAL : If user has valid access token and refresh token then it can be directly passed to the constructor
+	access_token:
+		'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkExMzgxMzU4Iiwicm9sZXMiOjAsInVzZXJ0eXBlIjoiVVNFUiIsImlhdCI6MTY3OTYzNzY3NywiZXhwIjoxNzY2MDM3Njc3fQ.INES_cGR-lbibUzvD7KcZm0e_LT-5DtP8A-Cdt1DIeLguL9NwDqGGODhYRmszehc2SH6ihArUwrINvfBORwwOg',
+	refresh_token:
+		'eyJhbGciOiJIUzUxMiJ9.eyJ0b2tlbiI6IlJFRlJFU0gtVE9LRU4iLCJpYXQiOjE2Nzk2Mzc2Nzd9.6VulqI2s_SwX0SJmftn9ATCneNyZFZHwTCDVDC-buPjlpJqNAEk3ZUEafgLT0zexEi-KJSfaeGV_mgib3tiydA',
 });
 
-// If user does not have valid access token and refresh token then use generateSession method 
-smart_api.generateSession("CLIENT_CODE", "PASSWORD") 
-    .then((data) => {
-        console.log(data)
-        //        return smart_api.getProfile()
+// // If user does not have valid access token and refresh token then use generateSession method
 
-        // User Methods
-        // return smart_api.logout()
+// }
+// smart_api
+// 	.generateSession('A1381358', '1258', '611433')
+// 	.then((data) => {
+// 		console.log(data);
+// 	//        return smart_api.getProfile()
 
-        // return smart_api.getRMS();
+// 		// 	// User Methods
+// 		// 	// return smart_api.logout()
 
-        // Order Methods
-        // return smart_api.placeOrder({
-        //     "variety": "NORMAL",
-        //     "tradingsymbol": "SBIN-EQ",
-        //     "symboltoken": "3045",
-        //     "transactiontype": "BUY",
-        //     "exchange": "NSE",
-        //     "ordertype": "LIMIT",
-        //     "producttype": "INTRADAY",
-        //     "duration": "DAY",
-        //     "price": "19500",
-        //     "squareoff": "0",
-        //     "stoploss": "0",
-        //     "quantity": "1"
-        // })
+// 		// 	// return smart_api.getRMS();
 
+// 		// 	// Order Methods
+// 		// 	// return smart_api.placeOrder({
+// 		// 	//     "variety": "NORMAL",
+// 		// 	//     "tradingsymbol": "SBIN-EQ",
+// 		// 	//     "symboltoken": "3045",
+// 		// 	//     "transactiontype": "BUY",
+// 		// 	//     "exchange": "NSE",
+// 		// 	//     "ordertype": "LIMIT",
+// 		// 	//     "producttype": "INTRADAY",
+// 		// 	//     "duration": "DAY",
+// 		// 	//     "price": "19500",
+// 		// 	//     "squareoff": "0",
+// 		// 	//     "stoploss": "0",
+// 		// 	//     "quantity": "1"
+// 		// 	// })
 
-        // return smart_api.modifyOrder({
-        //     "orderid": "201130000006424",
-        //     "variety": "NORMAL",
-        //     "tradingsymbol": "SBIN-EQ",
-        //     "symboltoken": "3045",
-        //     "transactiontype": "BUY",
-        //     "exchange": "NSE",
-        //     "ordertype": "LIMIT",
-        //     "producttype": "INTRADAY",
-        //     "duration": "DAY",
-        //     "price": "19500",
-        //     "squareoff": "0",
-        //     "stoploss": "0",
-        //     "quantity": "1"
-        // });
+// 		// 	// return smart_api.modifyOrder({
+// 		// 	//     "orderid": "201130000006424",
+// 		// 	//     "variety": "NORMAL",
+// 		// 	//     "tradingsymbol": "SBIN-EQ",
+// 		// 	//     "symboltoken": "3045",
+// 		// 	//     "transactiontype": "BUY",
+// 		// 	//     "exchange": "NSE",
+// 		// 	//     "ordertype": "LIMIT",
+// 		// 	//     "producttype": "INTRADAY",
+// 		// 	//     "duration": "DAY",
+// 		// 	//     "price": "19500",
+// 		// 	//     "squareoff": "0",
+// 		// 	//     "stoploss": "0",
+// 		// 	//     "quantity": "1"
+// 		// 	// });
 
-        // return smart_api.cancelOrder({
-        //     "variety": "NORMAL",
-        //     "orderid": "201130000006424"
-        // });
+// 		// 	// return smart_api.cancelOrder({
+// 		// 	//     "variety": "NORMAL",
+// 		// 	//     "orderid": "201130000006424"
+// 		// 	// });
 
-        // return smart_api.getOrderBook();
+// 		// 	// return smart_api.getOrderBook();
 
-        // return smart_api.getTradeBook();
+// 		// 	// return smart_api.getTradeBook();
 
+// 		// 	// Portfolio Methods
+// 		// 	// return smart_api.getHolding();
 
-        // Portfolio Methods
-        // return smart_api.getHolding();
+// 		// 	// return smart_api.getPosition();
 
-        // return smart_api.getPosition();
+// 		// 	// return smart_api.convertPosition({
+// 		// 	//     "exchange": "NSE",
+// 		// 	//     "oldproducttype": "DELIVERY",
+// 		// 	//     "newproducttype": "MARGIN",
+// 		// 	//     "tradingsymbol": "SBIN-EQ",
+// 		// 	//     "transactiontype": "BUY",
+// 		// 	//     "quantity": 1,
+// 		// 	//     "type": "DAY"
+// 		// 	// });
 
-        // return smart_api.convertPosition({
-        //     "exchange": "NSE",
-        //     "oldproducttype": "DELIVERY",
-        //     "newproducttype": "MARGIN",
-        //     "tradingsymbol": "SBIN-EQ",
-        //     "transactiontype": "BUY",
-        //     "quantity": 1,
-        //     "type": "DAY"
-        // });
+// 		// 	// GTT Methods
+// 		// 	// return smart_api.createRule({
+// 		// 	//    "tradingsymbol" : "SBIN-EQ",
+// 		// 	//    "symboltoken" : "3045",
+// 		// 	//    "exchange" : "NSE",
+// 		// 	//    "producttype" : "MARGIN",
+// 		// 	//    "transactiontype" : "BUY",
+// 		// 	//    "price" : 100000,
+// 		// 	//    "qty" : 10,
+// 		// 	//    "disclosedqty": 10,
+// 		// 	//    "triggerprice" : 200000,
+// 		// 	//    "timeperiod" : 365
+// 		// 	// })
+// 		// 	// return smart_api.modifyRule({
+// 		// 	//             "id" : 1000014,
+// 		// 	//             "symboltoken" : "3045",
+// 		// 	//             "exchange" : "NSE",
+// 		// 	//             "qty" : 10
 
+// 		// 	// })
+// 		// 	// return smart_api.cancelRule({
+// 		// 	//      "id" : 1000014,
+// 		// 	//      "symboltoken" : "3045",
+// 		// 	//      "exchange" : "NSE"
+// 		// 	// })
+// 		// 	// return smart_api.ruleDetails({
+// 		// 	//     "id" : 25
+// 		// 	// })
+// 		// 	// return smart_api.ruleList({
+// 		// 	//      "status" : ["NEW","CANCELLED"],
+// 		// 	//      "page" : 1,
+// 		// 	//      "count" : 10
+// 		// 	// })
 
-        // GTT Methods
-        // return smart_api.createRule({
-        //    "tradingsymbol" : "SBIN-EQ",
-        //    "symboltoken" : "3045",
-        //    "exchange" : "NSE", 
-        //    "producttype" : "MARGIN",
-        //    "transactiontype" : "BUY",
-        //    "price" : 100000,
-        //    "qty" : 10,
-        //    "disclosedqty": 10,
-        //    "triggerprice" : 200000,
-        //    "timeperiod" : 365
-        // })
-        // return smart_api.modifyRule({
-        //             "id" : 1000014,
-        //             "symboltoken" : "3045",
-        //             "exchange" : "NSE", 
-        //             "qty" : 10
-
-        // })
-        // return smart_api.cancelRule({
-        //      "id" : 1000014,
-        //      "symboltoken" : "3045",
-        //      "exchange" : "NSE"
-        // })
-        // return smart_api.ruleDetails({
-        //     "id" : 25
-        // })
-        // return smart_api.ruleList({
-        //      "status" : ["NEW","CANCELLED"],
-        //      "page" : 1,
-        //      "count" : 10 
-        // })
-
-        // Historical Methods
-        // return smart_api.getCandleData({
-        //     "exchange": "NSE",
-        //     "symboltoken": "3045",
-        //     "interval": "ONE_MINUTE",
-        //     "fromdate": "2021-02-10 09:00",
-        //     "todate": "2021-02-10 09:20"
-        // })
-    }).then((data) => {
-        console.log("PROFILE::", data)
-    })
-    .catch(ex => {
-        console.log("EX::", ex)
-    })
+// 		// 	// Historical Methods
+// 		// 	// return smart_api.getCandleData({
+// 		// 	//     "exchange": "NSE",
+// 		// 	//     "symboltoken": "3045",
+// 		// 	//     "interval": "ONE_MINUTE",
+// 		// 	//     "fromdate": "2021-02-10 09:00",
+// 		// 	//     "todate": "2021-02-10 09:20"
+// 		// 	// })
+// })
+// .then((data) => {
+// 	console.log('PROFILE::', data);
+// })
+// .catch((ex) => {
+// 	console.log('EX::', ex);
+// });
 
 // // // smart_api.generateToken("YOUR_REFRESH_TOKEN")
 // // //     .then((data) => {
@@ -133,18 +136,16 @@ smart_api.generateSession("CLIENT_CODE", "PASSWORD")
 
 // smart_api.setSessionExpiryHook(customSessionHook);
 
-
 // function customSessionHook() {
 //     // USER CAN GENERATE NEW JWT HERE
 //     console.log("User loggedout");
 // }
 
-
 // ########################### Socket Sample Code Starts Here ###########################
 // Old Websocket
 
 // let web_socket = new WebSocket({
-//     client_code: "CLIENT_CODE",   
+//     client_code: "CLIENT_CODE",
 //     feed_token: "FEED_TOKEN"
 // });
 
@@ -159,7 +160,6 @@ smart_api.generateSession("CLIENT_CODE", "PASSWORD")
 
 // web_socket.on('tick', receiveTick)
 
-
 // function receiveTick(data) {
 //     console.log("receiveTick:::::", data)
 // }
@@ -169,17 +169,16 @@ smart_api.generateSession("CLIENT_CODE", "PASSWORD")
 // ########################### Socket Sample Code Starts Here ###########################
 // New websocket
 
-
 // let web_socket = new WebSocketClient({
-//     clientcode: "CLIENT_CODE",    
-//     jwttoken: "jwt_token",
-//     apikey: "smartapi_key",
-//     feedtype: "order_feed",
+//     clientcode: "CLIENT_CODE",
+//     jwttoken: 'JWT_TOKEN',
+//     apikey: "API_KEY",
+//     feedtype: "FEED_TYPE",
 // });
 
 // web_socket.connect()
 //     .then(() => {
-//         web_socket.fetchData("ACTION_TYPE", "FEED_TYPE");  // ACTION_TYPE: subscribe | unsubscribe FEED_TYPE: order_feed
+//         web_socket.fetchData("subscribe", "order_feed");  // ACTION_TYPE: subscribe | unsubscribe FEED_TYPE: order_feed
 
 //         setTimeout(function () {
 //             web_socket.close()
@@ -188,9 +187,38 @@ smart_api.generateSession("CLIENT_CODE", "PASSWORD")
 
 // web_socket.on('tick', receiveTick);
 
-
 // function receiveTick(data) {
 //     console.log("receiveTick:::::", data);
 // }
 
-// ########################### Socket Sample Code Ends Here ###########################
+// ########################### Socket V2 Sample Code Ends Here ###########################
+// let web_socket = new WebSocketV2({
+// 	jwttoken:
+// 		'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IkExMzgxMzU4Iiwicm9sZXMiOjAsInVzZXJ0eXBlIjoiVVNFUiIsImlhdCI6MTY3OTYzNzY3NywiZXhwIjoxNzY2MDM3Njc3fQ.INES_cGR-lbibUzvD7KcZm0e_LT-5DtP8A-Cdt1DIeLguL9NwDqGGODhYRmszehc2SH6ihArUwrINvfBORwwOg',
+// 	apikey: 'm7TeW9kY',
+// 	clientcode: 'A1381358',
+// 	feedtype: '0838205854',
+// });
+
+// web_socket.connect().then((res) => {
+// 	let json_req = {
+// 		correlationID: 'abcde12345',
+// 		action: 1,
+// 		params: {
+// 			mode: 1,
+// 			tokenList: [
+// 				{
+// 					exchangeType: 2,
+// 					tokens: ['1232'],
+// 				},
+// 			],
+// 		},
+// 	};
+
+// 	web_socket.fetchData(json_req);
+// 	web_socket.on('tick', receiveTick);
+
+// 	function receiveTick(data) {
+// 		console.log('receiveTick:::::', data);
+// 	}
+// });
